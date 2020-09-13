a5fdcf7731ed727b82b3a2c0d4d57af2a8d7209e
aman-maharshi <amanm4harshi@gmail.com>
2020-09-13 12:50:35 +0530

    custom mobile menu toggle functionality and styles added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,11 @@ import "../styles/styles.css"
 if (module.hot) {
     module.hot.accept()
 }
+
+// custom mobile menu toggle functionality
+const toggleBtn = document.querySelector(".site-header__menu-icon")
+const mobileMenu = document.querySelector(".site-header__menu-content")
+
+toggleBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("toggle-menu")
+})
